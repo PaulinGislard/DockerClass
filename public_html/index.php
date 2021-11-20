e2d31d5097a4e18c664e2ef017ef69693d1cee3b
PaulinGislard <paulingislard@orange.fr>
2021-11-20 21:25:18 +0100

    r

--- public_html/index.php
+++ public_html/index.php
@@ -32,7 +32,7 @@ $username = "network";
 $password = "root";
 
 try {
-    $conn = new PDO("mysql:host=$servername;port=3306;", $username, $password); 
+    $conn = new PDO("mysql:host=$servername;port=3306;dbname=mysql", $username, $password); 
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
