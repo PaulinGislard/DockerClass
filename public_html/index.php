bb0642cb2a03646e33c40467cafa8fc6e228d8f2
PaulinGislard <paulingislard@orange.fr>
2021-11-20 18:51:50 +0100

    other

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
 <h1>Hello Cloudreach!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
-$host = 'localhost';
+/*$host = 'localhost:3306';
 $user = 'root';
 $pass = 'password';
 $connexion = new mysqli($host, $user, $pass);
@@ -10,5 +10,9 @@ if ($connexion->connect_errno) {
     printf("´Echec de la connexion : %s %s",
         $connexion->connect_errno, $connexion->connect_error);
     exit();}
-echo "cool";
+echo "cool";*/
+$db = new PDO('mysql:host=localhost;port=3306;dbname=testapp', 'root', 'password');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+var_dump($db->query('SELECT * from users'));
 ?>
