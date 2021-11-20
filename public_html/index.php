07b4a36314285d4fd7c5330eea0eb56b029baa6b
PaulinGislard <paulingislard@orange.fr>
2021-11-20 20:27:50 +0100

    ssv

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,12 @@
 <h1>Hello Cloudreach!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
+echo "cool"
 $host = 'mysql';
 $user = 'root';
 $pass = 'password';
 $connexion = new mysqli($host, $user, $pass);
-echo "cool"
+
 if ($connexion->connect_errno) {
     printf("´Echec de la connexion : %s %s",
         $connexion->connect_errno, $connexion->connect_error);
