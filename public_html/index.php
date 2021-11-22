46be2f6ce099962028ffa9273882d02c2abd140f
PaulinGislard <paulingislard@orange.fr>
2021-11-22 11:34:48 +0100

    mets goooo

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,41 @@
 <html>
     <head>
         <meta charset="utf-8"/>
+        <title>index</title>
+    </head>
+    <body>
+      <h1>Bienvenue  à vous</h1>
+      <form action="retourIndex.php" method="post">
+      <label>Identifiant : <input type="text" name="login" required></label>
+      <label>Mot de passe : <input type="password" name="motDePasse" required> </label>
+      </br>
+      <input type="submit" Value="Connexion"/>  
+      </form>
+      <style type="text/css">
+        input:invalid {
+        border: 2px dashed red;
+        }
+
+        input:valid {
+        border: 1px solid black;
+        }
+        body {
+        text-align: center;
+        }
+        h1{
+            font-size: 23px;
+
+        }
+        </style>
+      
+     
+    </body>
+</html>
+
+
+<html>
+    <head>
+        <meta charset="utf-8"/>
         <title> index </title>
     </head>
     <body><h1>Hello Cloudreach!</h1>
---
+++ public_html/retourIndex.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> RetourIndex </title>
+    </head>
+    <body>
+        <?php 
+        $login= $_POST['root'];
+        $motDePasse=$_POST['hello'];
+
+        $host = 'mysql';
+
+        // Database use name
+        $user = 'root';
+        
+        //database user password
+        $pass = 'hello';
+        $db_name ='Test';
+
+        $connexion = new mysqli($host, $user, $pass, $db_name );
+	if ($connexion->connect_errno) {
+		printf("´Echec de la connexion : %s %s",
+			$connexion->connect_errno, $connexion->connect_error);
+		exit();}
+	$connexion->set_charset("utf8");
+	$maRequete = "SELECT User, PWD FROM ID WHERE login LIKE '$login' AND motDePasse LIKE '$motDePasse';" ;
+	$result = $connexion->query($maRequete);
+	if(!$result) {
+		echo "la requete ne s’est pas exécutée";} 
+	else {
+        echo "la requete s’est bien passée";
+       
+        $resultat = $result->fetch_assoc(); // ...
+        $numeroUtilisateur = $resultat["numUtilisateur"];
+        if($numeroUtilisateur==1){
+            echo "<a href='menuAdministrateur.php'>cliquez ici</a><br>";
+        }else{
+            echo "<a href='listeConcours.php'>cliquez ici</a><br>";
+        }
+        session_start();
+        $_SESSION['utilisateur']=$numeroUtilisateur;
+		$result->free();}
+        $connexion->close();
+        
+      ?>
+      <br>
+
+<?php echo "Retour PHP"; ?>
+<br>
+<br>
+Identifiant: <?php echo $login; ?><br>
+Mot de passe: <?php echo $motDePasse; ?><br>
+Requete:  <?php echo $maRequete;?><br>
+numero: <?php echo $numeroUtilisateur;?>
+    </body>
+</html>e
